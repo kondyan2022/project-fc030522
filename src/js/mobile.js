c6dbb58e4c67b249413a26466b16beae51a97662
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-05-07 00:21:34 +0300

    Correct index.html

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -1,15 +1,15 @@
-+(() => {
+(() => {
   const refs = {
-    openMobileBtn: document.querySelector("[data-mobile-open]"),
-    closeMobileBtn: document.querySelector("[data-mobile-close]"),
-    mobile: document.querySelector("[data-mobile]"),
+    openMobileBtn: document.querySelector('[data-mobile-open]'),
+    closeMobileBtn: document.querySelector('[data-mobile-close]'),
+    mobile: document.querySelector('[data-mobile]'),
   };
 
-  refs.openMobileBtn.addEventListener("click", toggleMobile);
-  refs.closeMobileBtn.addEventListener("click", toggleMobile);
+  refs.openMobileBtn.addEventListener('click', toggleMobile);
+  refs.closeMobileBtn.addEventListener('click', toggleMobile);
 
   function toggleMobile() {
-    refs.mobile.classList.toggle("is-hidden");
-    document.body.classList.toggle("no-scroll");
+    refs.mobile.classList.toggle('is-hidden');
+    document.body.classList.toggle('no-scroll');
   }
 })();
