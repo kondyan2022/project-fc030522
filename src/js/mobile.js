6cda73b3a3d0d8042e4b057a098010bd400ec3c8
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-05-07 03:31:33 +0300

    Resolve problem burger menu

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -3,10 +3,12 @@
     openMobileBtn: document.querySelector('[data-mobile-open]'),
     closeMobileBtn: document.querySelector('[data-mobile-close]'),
     mobile: document.querySelector('[data-mobile]'),
+    links: document.querySelectorAll('a.burger-menu-navigation-link'),
   };
 
   refs.openMobileBtn.addEventListener('click', toggleMobile);
   refs.closeMobileBtn.addEventListener('click', toggleMobile);
+  refs.links.forEach(a => a.addEventListener('click', toggleMobile));
 
   function toggleMobile() {
     refs.mobile.classList.toggle('is-hidden');
