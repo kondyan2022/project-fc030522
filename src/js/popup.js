005262ed429d18b4b4654c993ed35aae00146b08
Vadym Prokhorenko <prokhorenko.vadim.98@gmail.com>
2023-05-09 02:30:23 +0300

    Add new event

--- src/js/popup.js
+++ src/js/popup.js
@@ -17,14 +17,40 @@ catalog.addEventListener("click", (event) => {
     const parentPicture = document.querySelector(".card-buy-watch");
     parentPicture.querySelector("picture").textContent = '';
     parentPicture.querySelector("picture").insertAdjacentHTML("afterbegin", clearClasses(parent));
-    parentPicture.querySelector("div>div>picture").classList.remove("catalog-list-item-picture");
-    parentPicture.querySelector("div>div>picture>img").classList.remove("catalog-list-item-img");
     document.querySelector(".modal-watch-name").textContent = event.target.textContent.trim();
     document.querySelector(".modal-watch-price")
         .textContent = parent.querySelector("span.catalog-list-item-span")
             .textContent.trim();
     change();
 })
+
+const saleProduct = document.querySelector(".sale-block-title");
+const codeElementPicture = `<picture>
+<source
+  srcset="./img/watches-8.webp 1x, ./img/watches-8-2x.webp 2x"
+  type="image/webp"
+/>
+<source
+  srcset="./img/watches-8.jpg 1x, ./img/watches-8-2x.jpg 2x"
+/>
+<img
+  src="./img/watches-8.jpg"
+  alt="Greenlane Watch"
+  width="276"
+  height="274"
+/>
+</picture>`;
+
+saleProduct.addEventListener("click", (event) => {
+    const parentPicture = document.querySelector(".card-buy-watch");
+    parentPicture.querySelector("picture").textContent = '';
+    parentPicture.querySelector("picture").insertAdjacentHTML("afterbegin", codeElementPicture);
+    document.querySelector(".modal-watch-name").textContent = event.target.textContent.trim();
+    document.querySelector(".modal-watch-price").textContent = "€700";
+    document.querySelector(".modal-watch-price").insertAdjacentHTML("beforeend", `<span class="modal-watch-sale">€1000</span>`);
+    change();
+})
+
 function clearClasses(element) {
     const string = element.querySelector("div>div>picture")?.innerHTML
     ?? element.querySelector("div>picture").innerHTML;
